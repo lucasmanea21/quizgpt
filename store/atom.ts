3fe4adb50e4ca34ea0184a2decf72f7439c46c60
lucasmanea21 <lucasmanea@Yahoo.com>
2023-07-04 14:33:37 +0300

    update: refactoring

--- store/atom.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from "jotai";
-
-export const gameStartTimeAtom = atom(1684325772638);
-export const userAnswerAtom = atom("");
-export const correctAnswerAtom = atom("");
-export const numCorrectAnswersAtom = atom(0);
-
-export const selectedAnswersAtom = atom([]);
-export const userProfileAtom = atom({});
--- utils/config.ts
+++
@@ -1,2 +0,0 @@
-export const API_URL = "http://localhost:8080";
-export const GAME_DURATION = 1000 * 1000;
